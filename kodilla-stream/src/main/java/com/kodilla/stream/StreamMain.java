fa6f21c1668c2d45ce989ae6612a214d318af33f
“FilaszkiewiczT <filaszkiewicz.t@gmail.com>
2018-02-15 21:45:29 +0100

    Zadanie 7.1 v1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.beautifier.PoemBeautifier;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -18,5 +19,14 @@ public class StreamMain {
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        //Zadanie 7.1
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        System.out.println(poemBeautifier.beautify("tekst nr 1", text -> "Tekst Nr 1"));
+        System.out.println(poemBeautifier.beautify("X", text -> text.replace("X", "New text")));
+        System.out.println(poemBeautifier.beautify("a,b,c,d,e", text -> text.toUpperCase()));
+        System.out.println(poemBeautifier.beautify("TOMEK", text -> text.toLowerCase()));
+        System.out.println(poemBeautifier.beautify("    Tekst     ", text -> text.trim()));
+        System.out.println(poemBeautifier.beautify("123X ", (text) -> text.substring(0).replace("X", "456789")));
     }
 }
