4e6fef002776a03565673e9b21683460fb058578
“FilaszkiewiczT <filaszkiewicz.t@gmail.com>
2018-03-02 11:52:11 +0100

    Zadanie 9.1 v2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieApp.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieApp.java
@@ -2,14 +2,17 @@ package com.kodilla.good.patterns.challenges;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MovieApp {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
 
         Map<String, List<String>> allMovies = movieStore.getMovies();
-        allMovies.entrySet().stream()
+        String titles = allMovies.entrySet().stream()
                 .flatMap(l -> l.getValue().stream())
-                .forEach(s -> System.out.println(s + "!"));
+                .collect(Collectors.joining("!"));
+
+        System.out.println(titles);
     }
 }
