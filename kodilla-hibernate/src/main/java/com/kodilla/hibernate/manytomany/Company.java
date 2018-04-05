9d225ce8f71de1ae585d24b3c4726163f9ceb04d
“FilaszkiewiczT <filaszkiewicz.t@gmail.com>
2018-04-05 21:54:52 +0200

    13.3 v3

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -0,0 +1,51 @@
+package com.kodilla.hibernate.manytomany;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "COMPANIES")
+public class Company {
+    @Id
+    @GeneratedValue
+    @Column(name = "COMPANY_ID")
+    private Long id;
+    @NotNull
+    @Column(name = "COMPANY_NAME")
+    private String name;
+    @ManyToMany (cascade = CascadeType.ALL, mappedBy = "companies")
+    private List<Employee> employees = new ArrayList<>();
+
+    public Company() {
+    }
+
+    public Company(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Employee.java
@@ -0,0 +1,66 @@
+package com.kodilla.hibernate.manytomany;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "EMPLOYEES")
+public class Employee {
+    @Id
+    @GeneratedValue
+    @Column(name = "EMPLOYEE_ID")
+    private Long id;
+    @NotNull
+    private String firstname;
+    @NotNull
+    private String lastname;
+    @ManyToMany (cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "JOIN_COMPANY_EMPLOYEE",
+            joinColumns = {@JoinColumn (name = "EMPLOYEE_ID", referencedColumnName = "EMPLOYEE_ID")},
+            inverseJoinColumns = {@JoinColumn (name = "COMPANY_ID", referencedColumnName = "COMPANY_ID")}
+    )
+    private List<Company> companies = new ArrayList<>();
+
+    public Employee() {
+    }
+
+    public Employee(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    private void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    private void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public List<Company> getCompanies() {
+        return companies;
+    }
+
+    public void setCompanies(List<Company> companies) {
+        this.companies = companies;
+    }
+}
